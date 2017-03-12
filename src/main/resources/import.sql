a066d6ed2f8bea23833e26eac610c382a21f1150
Mohammed Amine El Jirari <med.amineeljirari@gmail.com>
2017-03-12 19:28:57 +0100

    add import.sql for sample data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into Employee (id, firstName, lastName, jobFunction, salary, version) values (1, 'Mohammed Amine', 'El Jirari', 'Software Engineer', '50000', 1);
+insert into Employee (id, firstName, lastName, jobFunction, salary, version) values (2, 'Claire', 'Sinanian', 'Business Engineer', '75000', 1);
